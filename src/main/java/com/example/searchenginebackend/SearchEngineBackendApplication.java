37ec4590ed02d936bfe3915c12b7cac848437bbd
rohithprem18 <rohithprem91@gmail.com>
2026-01-30 11:31:05 +0530

    Initial

---
+++ src/main/java/com/example/searchenginebackend/SearchEngineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchenginebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/searchenginebackend/SearchEngineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchenginebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
