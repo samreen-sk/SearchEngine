cafbddd07062af746f29d53f48c97da33c1dfdd7
rohithprem18 <rohithprem91@gmail.com>
2026-02-01 22:31:50 +0530

    Initialised API endpoints for controller

--- src/main/java/com/example/searchenginebackend/controller/HealthController.java
+++ src/main/java/com/example/searchenginebackend/controller/HealthController.java
@@ -1,5 +1,13 @@
 package com.example.searchenginebackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
-    String name;
+
+    @GetMapping("/health")
+    public String health() {
+        return "Search Engine Backend is running ✅";
+    }
 }
